ab525c4d559d8a6bc0d87fd94315d58a8ca41b16
KeplerBoyce <keplerboy@gmail.com>
2022-08-16 18:37:20 -0700

    reduce computation for lookup tables

--- src/search.rs
+++ src/search.rs
@@ -143,52 +143,48 @@ pub fn search3x3_phase_2(cube3x3: Cube3x3, lookup: &HashMap<Cube3x3, Vec<Move>>,
 //generate lookup hashmap with specified depth from generic g1 state
 pub fn gen_lookups_3x3_phase_1(depth: usize) -> HashMap<Cube3x3Simple, Vec<Move>> {
     let mut hashmap = HashMap::new();
-    let mut deque: VecDeque<(Face, Face, Vec<Move>)> = VecDeque::from(vec!((Face::None, Face::None, Vec::new())));
+    let mut deque: VecDeque<(Cube3x3Simple, Face, Face, Vec<Move>)> = VecDeque::from(vec!((Cube3x3Simple::new(), Face::None, Face::None, Vec::new())));
     while deque.len() > 0 {
-        let (last_face, next_last_face, moves) = deque.pop_front().unwrap(); //destructure current state
+        let (cube, last_face, next_last_face, moves) = deque.pop_front().unwrap(); //destructure current state
 
-        let mut cube3x3 = Cube3x3Simple::new();
-        for m in moves.clone() {
-            cube3x3 = cube3x3.turn(m);
-        }
-        if hashmap.contains_key(&cube3x3) {
+        if hashmap.contains_key(&cube) {
             continue;
         } else {
-            hashmap.insert(cube3x3, moves.clone());
+            hashmap.insert(cube, moves.clone());
         }
         if moves.len() == depth {
             continue;
         }
-        //push next possible cube states (moves in move list are inverted)
+        //push next possible cube states
         if last_face != Face::F && !(last_face == Face::B && next_last_face == Face::F) {
-            deque.push_back((Face::F, last_face, moves.append_move(Move::F)));
-            deque.push_back((Face::F, last_face, moves.append_move(Move::Fi)));
-            deque.push_back((Face::F, last_face, moves.append_move(Move::F2)));
+            deque.push_back((cube.turn(Move::F), Face::F, last_face, moves.append_move(Move::F)));
+            deque.push_back((cube.turn(Move::Fi), Face::F, last_face, moves.append_move(Move::Fi)));
+            deque.push_back((cube.turn(Move::F2), Face::F, last_face, moves.append_move(Move::F2)));
         }
         if last_face != Face::R && !(last_face == Face::L && next_last_face == Face::R) {
-            deque.push_back((Face::R, last_face, moves.append_move(Move::R)));
-            deque.push_back((Face::R, last_face, moves.append_move(Move::Ri)));
-            deque.push_back((Face::R, last_face, moves.append_move(Move::R2)));
+            deque.push_back((cube.turn(Move::R), Face::R, last_face, moves.append_move(Move::R)));
+            deque.push_back((cube.turn(Move::Ri), Face::R, last_face, moves.append_move(Move::Ri)));
+            deque.push_back((cube.turn(Move::R2), Face::R, last_face, moves.append_move(Move::R2)));
         }
         if last_face != Face::U && !(last_face == Face::D && next_last_face == Face::U) {
-            deque.push_back((Face::U, last_face, moves.append_move(Move::U)));
-            deque.push_back((Face::U, last_face, moves.append_move(Move::Ui)));
-            deque.push_back((Face::U, last_face, moves.append_move(Move::U2)));
+            deque.push_back((cube.turn(Move::U), Face::U, last_face, moves.append_move(Move::U)));
+            deque.push_back((cube.turn(Move::Ui), Face::U, last_face, moves.append_move(Move::Ui)));
+            deque.push_back((cube.turn(Move::U2), Face::U, last_face, moves.append_move(Move::U2)));
         }
         if last_face != Face::B && !(last_face == Face::F && next_last_face == Face::B) {
-            deque.push_back((Face::B, last_face, moves.append_move(Move::B)));
-            deque.push_back((Face::B, last_face, moves.append_move(Move::Bi)));
-            deque.push_back((Face::B, last_face, moves.append_move(Move::B2)));
+            deque.push_back((cube.turn(Move::B), Face::B, last_face, moves.append_move(Move::B)));
+            deque.push_back((cube.turn(Move::Bi), Face::B, last_face, moves.append_move(Move::Bi)));
+            deque.push_back((cube.turn(Move::B2), Face::B, last_face, moves.append_move(Move::B2)));
         }
         if last_face != Face::L && !(last_face == Face::R && next_last_face == Face::L) {
-            deque.push_back((Face::L, last_face, moves.append_move(Move::L)));
-            deque.push_back((Face::L, last_face, moves.append_move(Move::Li)));
-            deque.push_back((Face::L, last_face, moves.append_move(Move::L2)));
+            deque.push_back((cube.turn(Move::L), Face::L, last_face, moves.append_move(Move::L)));
+            deque.push_back((cube.turn(Move::Li), Face::L, last_face, moves.append_move(Move::Li)));
+            deque.push_back((cube.turn(Move::L2), Face::L, last_face, moves.append_move(Move::L2)));
         }
         if last_face != Face::D && !(last_face == Face::U && next_last_face == Face::D) {
-            deque.push_back((Face::D, last_face, moves.append_move(Move::D)));
-            deque.push_back((Face::D, last_face, moves.append_move(Move::Di)));
-            deque.push_back((Face::D, last_face, moves.append_move(Move::D2)));
+            deque.push_back((cube.turn(Move::D), Face::D, last_face, moves.append_move(Move::D)));
+            deque.push_back((cube.turn(Move::Di), Face::D, last_face, moves.append_move(Move::Di)));
+            deque.push_back((cube.turn(Move::D2), Face::D, last_face, moves.append_move(Move::D2)));
         }
     }
     hashmap.insert(Cube3x3Simple::new(), Vec::new()); //include g1 state itself
@@ -198,44 +194,40 @@ pub fn gen_lookups_3x3_phase_1(depth: usize) -> HashMap<Cube3x3Simple, Vec<Move>
 //generate lookup hashmap with specified depth from solved state
 pub fn gen_lookups_3x3_phase_2(depth: usize) -> HashMap<Cube3x3, Vec<Move>> {
     let mut hashmap = HashMap::new();
-    let mut deque: VecDeque<(Face, Face, Vec<Move>)> = VecDeque::from(vec!((Face::None, Face::None, Vec::new())));
+    let mut deque: VecDeque<(Cube3x3, Face, Face, Vec<Move>)> = VecDeque::from(vec!((Cube3x3::new(), Face::None, Face::None, Vec::new())));
     while deque.len() > 0 {
-        let (last_face, next_last_face, moves) = deque.pop_front().unwrap(); //destructure current state
+        let (cube, last_face, next_last_face, moves) = deque.pop_front().unwrap(); //destructure current state
 
-        let mut cube3x3 = Cube3x3::new();
-        for m in moves.clone() {
-            cube3x3 = cube3x3.turn(m);
-        }
-        if hashmap.contains_key(&cube3x3) {
+        if hashmap.contains_key(&cube) {
             continue;
         } else {
-            hashmap.insert(cube3x3, moves.clone());
+            hashmap.insert(cube, moves.clone());
         }
         if moves.len() == depth {
             continue;
         }
-        //push next possible cube states (moves in move list are inverted)
+        //push next possible cube states
         if last_face != Face::F && !(last_face == Face::B && next_last_face == Face::F) {
-            deque.push_back((Face::F, last_face, moves.append_move(Move::F2)));
+            deque.push_back((cube.turn(Move::F2), Face::F, last_face, moves.append_move(Move::F2)));
         }
         if last_face != Face::R && !(last_face == Face::L && next_last_face == Face::R) {
-            deque.push_back((Face::R, last_face, moves.append_move(Move::R2)));
+            deque.push_back((cube.turn(Move::R2), Face::R, last_face, moves.append_move(Move::R2)));
         }
         if last_face != Face::U && !(last_face == Face::D && next_last_face == Face::U) {
-            deque.push_back((Face::U, last_face, moves.append_move(Move::U)));
-            deque.push_back((Face::U, last_face, moves.append_move(Move::Ui)));
-            deque.push_back((Face::U, last_face, moves.append_move(Move::U2)));
+            deque.push_back((cube.turn(Move::U), Face::U, last_face, moves.append_move(Move::U)));
+            deque.push_back((cube.turn(Move::Ui), Face::U, last_face, moves.append_move(Move::Ui)));
+            deque.push_back((cube.turn(Move::U2), Face::U, last_face, moves.append_move(Move::U2)));
         }
         if last_face != Face::B && !(last_face == Face::F && next_last_face == Face::B) {
-            deque.push_back((Face::B, last_face, moves.append_move(Move::B2)));
+            deque.push_back((cube.turn(Move::B2), Face::B, last_face, moves.append_move(Move::B2)));
         }
         if last_face != Face::L && !(last_face == Face::R && next_last_face == Face::L) {
-            deque.push_back((Face::L, last_face, moves.append_move(Move::L2)));
+            deque.push_back((cube.turn(Move::L2), Face::L, last_face, moves.append_move(Move::L2)));
         }
         if last_face != Face::D && !(last_face == Face::U && next_last_face == Face::D) {
-            deque.push_back((Face::D, last_face, moves.append_move(Move::D)));
-            deque.push_back((Face::D, last_face, moves.append_move(Move::Di)));
-            deque.push_back((Face::D, last_face, moves.append_move(Move::D2)));
+            deque.push_back((cube.turn(Move::D), Face::D, last_face, moves.append_move(Move::D)));
+            deque.push_back((cube.turn(Move::Di), Face::D, last_face, moves.append_move(Move::Di)));
+            deque.push_back((cube.turn(Move::D2), Face::D, last_face, moves.append_move(Move::D2)));
         }
     }
     hashmap.insert(Cube3x3::new(), Vec::new()); //include solved cube
